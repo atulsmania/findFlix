5e824864e77105e9fad0aa77ff1d2198f809b2e6
atulsmania <atul14ac@gmail.com>
2023-02-17 02:11:00 +0530

    updated package json tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,34 +10,10 @@ module.exports = {
         "max-md": { max: "767px" },
         "max-sm": { max: "639px" },
       },
-      colors: {
-        primary: {
-          50: "#5C8074",
-          100: "#537469",
-          200: "#425C54",
-          300: "#31453E",
-          400: "#202D29",
-          500: "#0F1513",
-        },
-      },
     },
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#7c81d3",
-          secondary: "#eeffad",
-          accent: "#a16ae8",
-          neutral: "#272B30",
-          "base-100": "#ECE9F1",
-          info: "#65BADC",
-          success: "#5DDAC5",
-          warning: "#FCC85A",
-          error: "#F7836E",
-        },
-      },
-    ],
+    themes: ["night", "cmyk"],
   },
 };
